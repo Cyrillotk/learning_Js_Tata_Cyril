c232db7a66746c30fff726fc5e3322d777f39b8f
cyrillotk <tatacyri1l@gmail.com>
2025-08-11 13:04:40 +0100

    num_guessing_game,html

--- day_2/test2.js
+++ day_2/test2.js
@@ -5,3 +5,18 @@ endYear.setFullYear(today.getFullYear());
 const msPerDay = 24 * 60 * 60 * 1000;// Number of milliseconds per day
 let daysLeft = (endYear.getTime() - today.getTime()) / msPerDay;
 daysLeft = Math.round(daysLeft)// returns days left in a year
+//times
+function JSClock() {
+    const time = new Date();
+    const hour = time.getHours();
+    const minute = time.getMinutes();
+    const second = time.getSeconds();
+    let temp = String(hour % 12);
+    if (temp === "0") {
+      temp = "12";
+    }
+    temp += (minute < 10 ? ":0" : ":") + minute;
+    temp += (second < 10 ? ":0" : ":") + second;
+    temp += hour >= 12 ? " P.M." : " A.M.";
+    return temp;
+  }
