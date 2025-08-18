7d0c616a2ddf6174b69fa3b388462e9acc1c7d50
cyrillotk <tatacyri1l@gmail.com>
2025-08-18 10:38:49 +0100

    generic methods

--- day_4/day_8/arrays.js
+++ day_4/day_8/arrays.js
@@ -24,4 +24,11 @@ function method(callbackFn, thisArg) {
       }
     }
   }
+  //generic methods
+  const arrayLike = {
+    0: "a",
+    1: "b",
+    length: 2,
+  };
+  console.log(Array.prototype.join.call(arrayLike, "+")); // 'a+b'
   
