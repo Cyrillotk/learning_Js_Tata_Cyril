80b9c0677335388ab28a5d39c6a5ce4f0d42511e
cyrillotk <tatacyri1l@gmail.com>
2025-08-19 14:55:39 +0100

    testing

---
+++ day_10/.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+    //buttons
+var quickAddBtn = document.getElementById("QuickAdd");
+var AddBtn = document.getElementById("Add");
+var cancelBtn = documment.getElementById("cancel")
+var quickAddFormDiv = document.querySelection('.quickForm'); 
+}
+// document.getElementByClassName('quickAddForm')
+
+// formFields
+var fullname = document.getElementById("fullname");
+var phone = document.getElementById("phone");
+var address = document.getElementById("address");
+var city = document.getElementById("city");
+var email = document.getElementById("email");
+
+// address book display
+var addBookDiv = document.querySelector(".addBook");
+
+//create storage array
+var addressBook = [];
+
+//Event listeners
+quickAddBtn.addEventListener("click",function(){
+    quickAddFormDiv.style.display = "block";
+});
+
+cancelBtn.add
